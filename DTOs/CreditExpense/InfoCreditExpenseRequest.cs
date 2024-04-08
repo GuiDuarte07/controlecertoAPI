c955417786118206b3c175f363ff04f78e2286e8
Guilherme Duarte <guilhduart.abr@gmail.com>
2024-04-08 10:37:27 -0300

    feat(creditExpense): CreditExpenseDTO

---
+++ DTOs/CreditExpense/InfoCreditExpenseRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace Finantech.DTOs.CreditCardExpense
+{
+    public class InfoCreditExpenseRequest
+    {
+        public double Amount { get; set; }
+        public string? Description { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public int InstallmentNumber { get; set; }
+        public string Destination { get; set; }
+        public int CreditPurchaseId { get; set; }
+    }
+}
