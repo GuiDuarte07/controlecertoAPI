9d6b2fc1c5e798008928ec71c53e499891aba1ff
Guilherme Duarte <guilhduart.abr@gmail.com>
2024-04-10 20:15:34 -0300

    feat(automapper): adding automapper to aplication

---
+++ Profiles/MapperProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Finantech.DTOs.Account;
+using Finantech.Models.Entities;
+
+namespace Finantech.Profiles
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile() 
+        {
+            CreateMap<CreateAccountRequest, Account>()
+                .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+                .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore())
+                .AfterMap((src, dest) =>
+                {
+                    dest.CreatedAt = DateTime.Now;
+                    dest.UpdatedAt = DateTime.Now;
+                });
+            CreateMap<Account, InfoAccountResponse>();
+        }
+    }
+}
